229e4465afc4452a5665300be7cc8a23d6fd1c1b
Pavan Varma <pavanvarma058@gmail.com>
2024-09-21 23:22:15 +0530

    modified even odd problem on Arrays section

--- 07-Array/code/src/SepEvenOdd.java
+++ 07-Array/code/src/SepEvenOdd.java
@@ -4,21 +4,39 @@ public class SepEvenOdd {
     public static void main(String[] args) {
         // Separate Even, Odd numbers in an array
         int[] numbers = {54, 43, 2, 5, 14, 17, 18, 19};
+//        int n = numbers.length;
+//        int[] ans = new int[n];
+//        int j = 0;
+//        for (int i=0; i<n; i++){
+//            if(numbers[i]%2==0){
+//                ans[j++] = numbers[i];
+//            }
+//        }
+//
+//        for (int i=0; i<n; i++){
+//            if(numbers[i]%2!=0){
+//                ans[j++] = numbers[i];
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(ans));
+        System.out.println(Arrays.toString(EvenOdd(numbers)));
+    }
+    public static int[] EvenOdd(int[] numbers){
         int n = numbers.length;
         int[] ans = new int[n];
         int j = 0;
-        for (int i=0; i<n; i++){
-            if(numbers[i]%2==0){
-                ans[j++] = numbers[i];
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                ans[j++] = number;
             }
         }
 
-        for (int i=0; i<n; i++){
-            if(numbers[i]%2!=0){
-                ans[j++] = numbers[i];
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                ans[j++] = number;
             }
         }
-
-        System.out.println(Arrays.toString(ans));
+        return ans;
     }
 }
