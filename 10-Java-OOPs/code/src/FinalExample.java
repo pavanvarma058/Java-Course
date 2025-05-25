a80f12613b7b4b03aae42cc9fa5379109ae4db8b
Pavan Varma <pavanvarma058@gmail.com>
2025-05-25 14:55:35 +0530

    Learned about final in java

---
+++ 10-Java-OOPs/code/src/FinalExample.java
@@ -0,0 +1,7 @@
+public class FinalExample {
+    public static void main(String[] args) {
+        final int x; // blank final variable because not initialized
+        x = 100;
+        System.out.println(x);
+    }
+}
