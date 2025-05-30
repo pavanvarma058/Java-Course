7b7939d7df203e36e21cfb12211548ae01785b8c
Pavan Varma <pavanvarma058@gmail.com>
2025-05-30 14:20:33 +0530

    added syntax of arrayList

--- 12-ArrayList/code/src/Main.java
+++ 12-ArrayList/code/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // Syntax of arrayList
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        System.out.println(arr);
     }
 }
