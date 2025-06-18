5100bc050a3bd7ddcd2779bdbc066e9b65b09350
Pavan Varma <pavanvarma058@gmail.com>
2025-06-18 15:09:48 +0530

    Completed the smaller Elements program

--- 12-ArrayList/code/src/SmallerElements.java
+++ 12-ArrayList/code/src/SmallerElements.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SmallerElements {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 80, 25, 65, 55, 14};
+        List<Integer> li = getSmaller(arr, 30);
+        for(Integer i : li){
+            System.out.println(i);
+        }
+    }
+    public static List<Integer> getSmaller(int[] arr, int k){
+        List<Integer> al = new ArrayList<Integer>();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] < k){
+                al.add(arr[i]);
+            }
+        }
+        return al;
+    }
 }
