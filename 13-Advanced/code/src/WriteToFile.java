e5cf63671a277deb9c7c47e38bd3c8ca7c0ba3bd
Pavan Varma <pavanvarma058@gmail.com>
2025-06-30 23:22:17 +0530

    Added some comments to the program

--- 13-Advanced/code/src/WriteToFile.java
+++ 13-Advanced/code/src/WriteToFile.java
@@ -5,9 +5,11 @@ public class WriteToFile {
     public static void main(String[] args) {
         // Writing to a file
         try {
+            // Specify the file path
             FileWriter writer = new FileWriter("D:\\File.txt");
+            // Write data to file
             writer.write("Java is a programming language");
-
+            // Close the writer
             writer.close();
 
             System.out.println("Successfully wrote to file");
